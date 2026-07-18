35c68b8d73a7ebf2b33c744e282e4e5993169625
Craig <craig@nykindale.com>
2026-07-18 15:08:39 +0100

    Added list modal. Entries can be clicked to centre the map to the entry's coordinates.

--- src/Web/list.ts
+++ src/Web/list.ts
@@ -1,3 +1,16 @@
 function initList() {
+    const checkinElements = document.querySelectorAll(".journal-entry");
 
+    checkinElements.forEach(c => {
+        c.addEventListener("click", checkinElementClick);
+    });
+}
+
+function checkinElementClick(event: any) {
+    map
+        .setView(
+            [
+                event.currentTarget.dataset.latitude,
+                event.currentTarget.dataset.longitude
+            ], 3);
 }
--- src/wwwroot/dist/scripts/list.js
+++ src/wwwroot/dist/scripts/list.js
@@ -1,3 +1,14 @@
 "use strict";
 function initList() {
+    const checkinElements = document.querySelectorAll(".journal-entry");
+    checkinElements.forEach(c => {
+        c.addEventListener("click", checkinElementClick);
+    });
+}
+function checkinElementClick(event) {
+    map
+        .setView([
+        event.currentTarget.dataset.latitude,
+        event.currentTarget.dataset.longitude
+    ], 3);
 }
