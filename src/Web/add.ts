fcf1902590580ddb413cb27a44fe07a8f53cc79d
Craig <craig@nykindale.com>
2026-07-20 20:42:57 +0100

    Added file input to add form. On choosing an image, its metadata is read, and populates the date, timezone and coordinates fields on the form. Fixed an issue where the new marker would not disappear when dialog closed. Closes #5 #12

--- src/Web/add.ts
+++ src/Web/add.ts
@@ -1,3 +1,6 @@
+declare const ExifReader: any;
+let newMarker: any = null;
+
 async function initAdd() {
     var greenIcon = new L.Icon({
         iconUrl: "lib/leaflet/images/marker-icon-2x-green.png",
@@ -10,18 +13,12 @@ async function initAdd() {
 
     const latitude = getRequiredElementById<HTMLInputElement>("lat")
     const longitude = getRequiredElementById<HTMLInputElement>("long");
-    const form = getRequiredElementById("checkin-form");
-
-    if (!latitude || !longitude) {
-        throw new Error("#latitude or #longitude element not found.");
-    }
-
-    if (!form) {
-        throw new Error("Form not found");
-    }
+    const img = getRequiredElementById<HTMLInputElement>("img");
+    const dateTime = getRequiredElementById<HTMLInputElement>("datetime");
 
     latitude.addEventListener("input", updateMap);
     longitude.addEventListener("input", updateMap);
+    img.addEventListener("input", updateImg);
 
     const timeZoneDataList = getRequiredElementById<HTMLDataListElement>("timezoneList");
     const timeZoneInput = getRequiredElementById<HTMLInputElement>("timezoneInput");
@@ -42,6 +39,7 @@ async function initAdd() {
 
         option.value = tz;
         option.label = `(${offset}) ${tz}`;
+        option.dataset.offset = offset.slice(-6);
 
         timeZoneDataList.appendChild(option);
     });
@@ -80,8 +78,6 @@ async function initAdd() {
         }
     );
 
-    let newMarker: any = null;
-
     function updateMap() {
         const latitudeElement = getRequiredElementById<HTMLInputElement>("lat");
         const longitudeElement = getRequiredElementById<HTMLInputElement>("long");
@@ -92,12 +88,72 @@ async function initAdd() {
         if (!isNaN(lat) && !isNaN(lng)) {
             const newLatLng = [lat, lng];
 
-            if(!newMarker) {
-               newMarker = L.marker(newLatLng, { icon: greenIcon }).addTo(map); 
+            if (!newMarker) {
+                newMarker = L.marker(newLatLng, { icon: greenIcon }).addTo(map);
             }
 
             newMarker.setLatLng(newLatLng);
             map.setView(newLatLng, 6);
         }
     }
+
+    async function updateImg(event: any) {
+        const e = event.target as HTMLInputElement;
+        const files = e.files;
+
+        if (!files) {
+            return;
+        }
+
+        const tags = await ExifReader.load(files[0]);
+        const dateCreated = tags['DateCreated'].description;
+        const gpsLatitudeUnsigned = tags['GPSLatitude'].description;
+        const gpsLongitudeUnsigned = tags['GPSLongitude'].description;
+        const gpsLongitudeRef = tags['GPSLongitudeRef'].description;
+        const gpsLatitudeRef = tags['GPSLatitudeRef'].description;
+        // const gpsAltitude = tags['GPSAltitude'].description;
+
+        const offset = dateCreated.slice(-6);
+        let gpsLatitude = gpsLatitudeUnsigned;
+        let gpsLongitude = gpsLongitudeUnsigned;
+
+        // console.log(tags);
+        // console.log(dateCreated.slice(0, 19));
+        // console.log('TZ', dateCreated.slice(-6));
+        // console.log('Lat', gpsLatitude, 'Long', gpsLongitude);
+        // console.log('Altitude', gpsAltitude);
+
+        // TODO: Finish this off, and make gpsLongitudeRef const again
+        if (gpsLongitudeRef === "West longitude") {
+            gpsLongitude = `-${gpsLongitude}`;
+        }
+
+        if (gpsLatitudeRef === 'South latitude') {
+            gpsLatitude = `-${gpsLatitude}`;
+        }
+
+        latitude.value = gpsLatitude;
+        longitude.value = gpsLongitude;
+        dateTime.value = dateCreated.slice(0, 19);
+
+        const tzItem = (document
+            .querySelector<HTMLInputElement>(
+                `#timezoneList [data-offset="${offset}"]`
+            ) as HTMLInputElement).value;
+
+        if(!tzItem) {
+            return;
+        }
+
+        timeZoneInput.value = tzItem;
+
+        updateMap();
+    }
+}
+
+function closeAdd() {
+    newMarker.remove();
+    newMarker = null;
+
+    clearModals();
 }
--- src/wwwroot/dist/scripts/add.js
+++ src/wwwroot/dist/scripts/add.js
@@ -1,4 +1,5 @@
 "use strict";
+let newMarker = null;
 async function initAdd() {
     var greenIcon = new L.Icon({
         iconUrl: "lib/leaflet/images/marker-icon-2x-green.png",
@@ -10,15 +11,11 @@ async function initAdd() {
     });
     const latitude = getRequiredElementById("lat");
     const longitude = getRequiredElementById("long");
-    const form = getRequiredElementById("checkin-form");
-    if (!latitude || !longitude) {
-        throw new Error("#latitude or #longitude element not found.");
-    }
-    if (!form) {
-        throw new Error("Form not found");
-    }
+    const img = getRequiredElementById("img");
+    const dateTime = getRequiredElementById("datetime");
     latitude.addEventListener("input", updateMap);
     longitude.addEventListener("input", updateMap);
+    img.addEventListener("input", updateImg);
     const timeZoneDataList = getRequiredElementById("timezoneList");
     const timeZoneInput = getRequiredElementById("timezoneInput");
     const timezones = Intl.supportedValuesOf("timeZone");
@@ -34,6 +31,7 @@ async function initAdd() {
         const offset = getTimeZoneOffset(tz);
         option.value = tz;
         option.label = `(${offset}) ${tz}`;
+        option.dataset.offset = offset.slice(-6);
         timeZoneDataList.appendChild(option);
     });
     timeZoneInput.value = Intl.DateTimeFormat().resolvedOptions().timeZone;
@@ -49,7 +47,6 @@ async function initAdd() {
         const iso = getIso8601DateString(tz, dateTime);
         e.detail.parameters.datetime = iso;
     });
-    let newMarker = null;
     function updateMap() {
         const latitudeElement = getRequiredElementById("lat");
         const longitudeElement = getRequiredElementById("long");
@@ -64,4 +61,48 @@ async function initAdd() {
             map.setView(newLatLng, 6);
         }
     }
+    async function updateImg(event) {
+        const e = event.target;
+        const files = e.files;
+        if (!files) {
+            return;
+        }
+        const tags = await ExifReader.load(files[0]);
+        const dateCreated = tags['DateCreated'].description;
+        const gpsLatitudeUnsigned = tags['GPSLatitude'].description;
+        const gpsLongitudeUnsigned = tags['GPSLongitude'].description;
+        const gpsLongitudeRef = tags['GPSLongitudeRef'].description;
+        const gpsLatitudeRef = tags['GPSLatitudeRef'].description;
+        // const gpsAltitude = tags['GPSAltitude'].description;
+        const offset = dateCreated.slice(-6);
+        let gpsLatitude = gpsLatitudeUnsigned;
+        let gpsLongitude = gpsLongitudeUnsigned;
+        // console.log(tags);
+        // console.log(dateCreated.slice(0, 19));
+        // console.log('TZ', dateCreated.slice(-6));
+        // console.log('Lat', gpsLatitude, 'Long', gpsLongitude);
+        // console.log('Altitude', gpsAltitude);
+        // TODO: Finish this off, and make gpsLongitudeRef const again
+        if (gpsLongitudeRef === "West longitude") {
+            gpsLongitude = `-${gpsLongitude}`;
+        }
+        if (gpsLatitudeRef === 'South latitude') {
+            gpsLatitude = `-${gpsLatitude}`;
+        }
+        latitude.value = gpsLatitude;
+        longitude.value = gpsLongitude;
+        dateTime.value = dateCreated.slice(0, 19);
+        const tzItem = document
+            .querySelector(`#timezoneList [data-offset="${offset}"]`).value;
+        if (!tzItem) {
+            return;
+        }
+        timeZoneInput.value = tzItem;
+        updateMap();
+    }
+}
+function closeAdd() {
+    newMarker.remove();
+    newMarker = null;
+    clearModals();
 }
